db4b840359e3483b25db79950bac3dc21d6b16b3
Pradhyum Vyas <pradhyumvyas92@gmail.com>
2023-11-18 01:43:27 +0530

    Utlis code added

---
+++ Readme.md
@@ -0,0 +1,6 @@
+## NPM package used
+- CORS
+- cookie-parser ==> For setting or updating cookie at client side;
+- dotenv ==> For handling environment variables
+- express
+- mongoose
--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
 // require('dotenv').config({path:'./env'})
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({
    path:'./env'
 })
 
 connectDB()
+.then(()=>{
+
+   app.on(('error'),(err)=>{
+      console.log("Error: ", error);
+      throw error;
+     })
+     
+   app.listen(process.env.PORT || 8000,()=>{
+      console.log("App is listen on ", process.env.PORT);
+     })
+})
+.cach((error)=>{
+   console.error("Mongo DB connection failed !!: ", error);
+   process.exit(1);
+})
 /*
 import express from "express";
 const app = express();
