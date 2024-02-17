3a221a76e6f5688b025a91dc7b03f20831a7c3b4
Pradhyum Vyas <pradhyumvyas92@gmail.com>
2024-02-18 01:36:56 +0530

    add router for user module

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,17 @@
 import { Router } from "express";
-import { registerUser, loginUser, logoutUser,refreshToken } from "../controllers/user.controller.js";
+import { 
+   registerUser, 
+   loginUser, 
+   logoutUser,
+   refreshToken, 
+   changeCurrentPassword, 
+   getCurrentUser, 
+   updateAccountDetails, 
+   updateUserAvatar, 
+   updateUserCoverImage, 
+   getUserChannelProfile, 
+   getWatchHiistory } 
+   from "../controllers/user.controller.js";
 import {upload}  from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
@@ -23,5 +35,14 @@ router.route("/login").post(loginUser);
 // Secured Routes
 router.route("/logout").post(verifyJWT, logoutUser)
 router.route("/refresh-token").post(refreshToken)
+router.route("/change-password").post(verifyJWT, changeCurrentPassword)
+router.route("/current-user").post(verifyJWT, getCurrentUser)
+router.route("/update-detail").patch(verifyJWT, updateAccountDetails)
+
+router.route("/update-avatar").patch(verifyJWT, upload.single("avatar"), updateUserAvatar)
+router.route("/updater-cover-image").patch(verifyJWT, upload.single("/coverImage"), updateUserCoverImage)
+
+router.route("/channel/:username").get(verifyJWT, getUserChannelProfile)
+router.route("/watch-hostory").get(verifyJWT, getWatchHiistory)
 
 export default router;
